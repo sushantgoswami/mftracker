3c4760dad8b151020e964f9cbbc1c96cf3ff8e9c
Cloud User <SuperUserNS81@tagdeal-zabbix02.qhgdufjuueyulbyp41dhdbi0gh.cwx.internal.cloudapp.net>
2026-08-31 14:11:08 +0000

    v22

--- mftracker-v01/installer/default_user_table.php
+++ mftracker-v01/installer/default_user_table.php
@@ -3,7 +3,7 @@ session_start();
 include '../db_connect.php';
 
 // 5. Construct the SQL query with backticks around the variable
-$sql = "CREATE TABLE `users` (
+$sql1 = "CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -11,21 +11,39 @@ $sql = "CREATE TABLE `users` (
   `email` varchar(50) NOT NULL,
   `tablename` varchar(50) NOT NULL,
   `question1` varchar(35) NOT NULL,
-  `answer1` varchar(15) NOT NULL
+  `answer1` varchar(15) NOT NULL,
+  `last_invested_value` float(12,2) NOT NULL DEFAULT 0.00,
+  `purchase_total_value` float(12,2) NOT NULL DEFAULT 0.00,
+  `last_total_value` float(12,2) NOT NULL DEFAULT 0.00,
+  `current_total_value` float(12,2) NOT NULL DEFAULT 0.00
 )";
+$stmt1 = $conn->prepare($sql1);
+$stmt1->execute();
+$stmt1->close();
 
-// 6. Prepare and execute the query using the statement object
-$stmt1 = $conn->prepare($sql);
+$sql2 = "ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `username_2` (`username`)";
+$stmt1 = $conn->prepare($sql2);
+$stmt1->execute();
+$stmt1->close();
 
-if ($stmt1->execute()) {
-    $_SESSION['msg'] = "Registration Successful, Data Table Creation Successful";
-} else {
-    $_SESSION['msg'] = "Registration Successful, Data Table Creation is not Successful";
-}
+$sql3 = "ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT";
+$stmt1 = $conn->prepare($sql3);
+$stmt1->execute();
+$stmt1->close();
+
+
+// if ($stmt1->execute()) {
+//     $_SESSION['msg'] = "Registration Successful, Data Table Creation Successful";
+// } else {
+//     $_SESSION['msg'] = "Registration Successful, Data Table Creation is not Successful";
+// }
 
 // 7. Close connections
 $conn->close();
-$stmt1->close();
+// $stmt1->close();
 header("Location: ../../index.php");
 exit;
 ?>
